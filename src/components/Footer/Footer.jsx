09961edef348d08bc6aa402a0b185669d4d45a10
Edward Donald <donald808@icloud.com>
2023-06-12 21:10:39 +0100

    header and footer complete

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+        <div className='footer-nav'>
+        <p>About Us</p>
+        <p>Terms & Conditions</p>
+        <p>Privacy & Cookie Policies</p>
+        </div>
+        <div className='tags'>
+        <p>2022</p>
+        <p>UniLife</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React, {useContext} from 'react'
+import './Header.css'
+import { Link } from 'react-router-dom'
+import { CiHeart, CiMail } from "react-icons/ci";
+import { BsHouses } from "react-icons/bs";
+
+
+
+function Header() {
+  return (
+    <div className='header-container'>
+        <div className='header'>
+       <BsHouses className='sizing'/><h1>UniLife</h1>
+       </div>
+        <div className='nav-container'>
+        <CiHeart className='sizing' /><p>Shortlist</p>
+        <CiMail className='sizing' /><p>Contact Us</p>
+        </div>
+
+        
+        
+    </div>
+  )
+}
+
+export default Header
